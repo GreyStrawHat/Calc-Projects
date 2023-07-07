8eefa47397fdea4a02e02c2c14b6bdd356abda46
isiah.nembhard <isiah.l.nembhard@army.mil>
2023-07-07 00:35:01 -0400

    Added doxygen comments

--- 0_Common/include/header-files/default_case.h
+++ 0_Common/include/header-files/default_case.h
@@ -3,6 +3,20 @@
 
 #include <stdint.h>
 
+/**
+ * @brief Performs calculations on two operands based on the provided bitwise operator.
+ *
+ * The function takes two operands and an operator as inputs and performs the
+ * specified mathematical operation.If an unsupported operator is provided,
+ * the function returns an error.
+ *
+ * @param operand1 The first operand as a string.
+ * @param operator_val The operator as a string.
+ * @param operand2 The second operand as a string.
+ * @param uarg_1 A pointer to an unsigned 32-bit integer to store the first operand.
+ * @param uarg_2 A pointer to an unsigned 32-bit integer to store the second operand.
+ * @return Returns 0 if operation is successful. Returns 1 in case of any errors.
+ */
 int default_case(char *     operand1,
                   char *     operator_val,
                   char *     operand2,
