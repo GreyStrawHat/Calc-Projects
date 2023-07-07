1c4dcf9c95c788a6a351a79b5d587da09fe761fc
isiah.nembhard <isiah.l.nembhard@army.mil>
2023-07-07 00:58:47 -0400

    Fixed up some input sanitization

--- 1_SimpleCalc/src/source-files/simplecalc_main.c
+++ 1_SimpleCalc/src/source-files/simplecalc_main.c
@@ -7,32 +7,36 @@
 
 int main(int argc, char ** argv)
 {
-    int error_status = 0;
+    char ** end_pp       = NULL;
+    int     error_status = 0;
+    int32_t arg1_test    = strtol(argv[1], end_pp, BASE_TEN);
+    int32_t arg3_test    = strtol(argv[3], end_pp, BASE_TEN);
+    int32_t arg1_length  = strlen(argv[1]);
+    int32_t arg3_length  = strlen(argv[3]);
 
     if (EQUATION_COUNT != argc)
     {
         fprintf(stderr, "Invalid number of arguments.\n\n");
         usage(argv[0]);
-        error_status = 1;
+        error_status = -1;
         goto END;
     }
 
-    char ** end_pp = NULL;
-
     if (((0 == strtoul(argv[1], end_pp, BASE_TEN)) &&
-        (0 != strncmp(argv[1], "0\n", 1))) || ((0 == strtoul(argv[3], end_pp, BASE_TEN)) &&
-        (0 != strncmp(argv[3], "0\n", 1))))
+         (0 != strncmp(argv[1], "0\n", 1))) ||
+        ((0 == strtoul(argv[3], end_pp, BASE_TEN)) &&
+         (0 != strncmp(argv[3], "0\n", 1))) || (arg1_test != arg1_length) || (arg3_test != arg3_length))
     {
         fprintf(stderr, "Integer values only.\n\n");
         usage(argv[0]);
-        error_status = 1;
+        error_status = -1;
         goto END;
     }
 
     if (-1 == calc(argv[1], argv[2], argv[3]))
     {
         usage(argv[0]);
-        error_status = 1;
+        error_status = -1;
         goto END;
     }
 
